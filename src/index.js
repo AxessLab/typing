fecd2e6ee8532c398f867d112d8fcd94e7394006
Erik Zetterström <erik@ictenablers.com>
2019-09-04 21:32:05 +0200

    New test build for trying label andautofocus with screenreaders.

--- src/index.js
+++ src/index.js
@@ -4,41 +4,65 @@ import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
-class TypeMe extends React.Component {
-    render() {
-      return (
-        <h1>Try the H shortcut on Jaws</h1>
-      );
+  class Task extends React.Component {
+    constructor(props) {
+      super(props);
+      this.currentPosition=0;
+      this.errors=0;
+      this.state = { typedValue : "", 
+                     doneText : "",
+                     remainingText : props.currentText } 
     }
-  }
-  
-  class TypeHere extends React.Component {
+
+    handleChange = e => {
+      this.setState({typedValue: e.target.value});
+    }
 
     handleKey = e => {
-        switch(e.key) { 
-            case 'R': //Jaws regions
-            case 'Q': //Jaws main content
-            case 'H': //Jaws heading shortcut
-            case '1': //Jaws heading 1-6 shortcut
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6': 
-            case 'L': //Jaws lists
-            case 'I': //JAws items
-                console.log("Trying to supress "+e.key);
-                e.stopPropagation();
-                break;
-            default:
-                
+      console.log("Pressed: "+e.key+" = "+this.props.currentText.charAt(this.currentPosition)+"?");
+      if(e.which !== 0 && !(e.key==="Control") && !(e.key==="Meta") && !(e.key==="Shift") && !(e.key==="Alt")) { //igore modifiers for now, probably bad code
+        if(e.key==this.props.currentText.charAt(this.currentPosition)) { //Correct, play feedback (maybe), update progress and move to the next letter
+          this.currentPosition++;
+          console.log("Current position: "+this.currentPosition+" length: "+this.props.currentTextLength);
+          if(this.currentPosition==this.props.currentTextLength) { //Task complete, play feedback, start next task
+            if(this.errors==0) {
+                alert("Jättebra jobbat! Felfri!");
+            }
+            else {
+                alert("Bra jobbat! Bara "+this.errors+" fel.");
+            }
+            this.currentPosition=0; 
+            this.errors=0;
+            this.setState({typedValue: ""});
+          }
+          //Update progress
+          const tmpDone = this.props.currentText.substr(0,this.currentPosition);
+          const tmpRemaining = this.props.currentText.substr(this.currentPosition,this.props.currentTextLength);
+          this.setState({ doneText: tmpDone});
+          this.setState({ remainingText: tmpRemaining}); 
         }
-        
+        else { //Incorrect, play feedback, remove last letter from input
+          this.errors++;
+          const tmpStr = this.state.typedValue;
+          const cutStr = tmpStr.substr(0, tmpStr.length-1);
+          this.setState({typedValue: cutStr});
+        }
+      }
     }
+
     render() {
       return (
-        <input type="text" name="TypeHereForm" onKeyDown={this.handleKey}></input>
+        <label><span style={{color: 'white', backgroundColor: 'black'}}>{this.state.doneText}</span>
+        <span>{this.state.remainingText}</span><br></br>
+        <input type="text" 
+              name="TypeHereForm"
+              id="typeHereId" 
+              value={this.state.typedValue} 
+              onChange={this.handleChange}
+              onKeyUp={this.handleKey}
+              autoFocus>
+        </input>
+        </label>
       );
     }
 
@@ -46,23 +70,19 @@ class TypeMe extends React.Component {
 
   
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.currentText="fjfj"; //Init training task
+    }
+
     render() {
       return (
-        <div className="game">
-          <div className="type-me">
-            <TypeMe />
-          </div>
+        <div className="game" style={{
+          position: 'absolute', left: '50%', top: '50%',
+          transform: 'translate(-50%, -50%)'
+      }}>
           <div className="type-here">
-            <TypeHere />
-          </div>
-          <div>
-            <h1>Heading 1A</h1>
-            <h2>Heading 2</h2>
-            <h3>Heading 3</h3>
-            <h4>Heading 4</h4>
-            <h5>Heading 5</h5>
-            <h6>Heading 6</h6>
-            <h1>Heading 1B</h1>
+            <Task currentText={this.currentText} currentTextLength={this.currentText.length}/>
           </div>
         </div>
       );
