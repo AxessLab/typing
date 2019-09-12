52f6dae7ded96e87b8eff988b2ec74fbe970f3bc
duff-a11y <joakim@axesslab.com>
2019-09-12 16:36:24 +0200

    Changed from input to div and tested role='application'

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,22 +4,30 @@ import './Task.scss';
 class Task extends React.Component {
   constructor(props) {
     super(props);
-    this.currentPosition=0;
+    this.input = React.createRef();
+    this.currentPosition = 0;
     this.errors=0;
-    this.state = { typedValue : "", 
-                   doneText : "",
-                   remainingText : props.currentText } 
+    this.state = { 
+      typedValue : "",
+      valueToType: props.currentText.charAt(this.currentPosition), 
+      doneText : "",
+      remainingText : props.currentText.substr(1,this.props.currentTextLength),
+    } 
   }
 
-  handleChange = e => {
-    this.setState({typedValue: e.target.value});
+  componentDidMount() {
+    this.input.current.focus();
   }
 
   handleKey = e => {
     console.log("Pressed: "+e.key+" = "+this.props.currentText.charAt(this.currentPosition)+"?");
     if(e.which !== 0 && !(e.key==="Control") && !(e.key==="Meta") && !(e.key==="Shift") && !(e.key==="Alt")) { //igore modifiers for now, probably bad code
-      if(e.key===this.props.currentText.charAt(this.currentPosition)) { //Correct, play feedback (maybe), update progress and move to the next letter
+      if(e.key=== this.props.currentText.charAt(this.currentPosition)) { //Correct, play feedback (maybe), update progress and move to the next letter
         this.currentPosition++;
+        this.setState({
+          typedValue: this.state.typedValue + e.key,
+          valueToType: this.props.currentText.charAt(this.currentPosition)
+        });
         console.log("Current position: "+this.currentPosition+" length: "+this.props.currentTextLength);
         if(this.currentPosition===this.props.currentTextLength) { //Task complete, play feedback, start next task
           this.errors === 0 ? alert("Jättebra jobbat! Felfri!") :  alert("Bra jobbat! Bara "+this.errors+" fel.");
@@ -27,18 +35,19 @@ class Task extends React.Component {
           //Resetting values
           this.currentPosition=0; 
           this.errors=0;
-          this.setState({typedValue: ""});
+          this.setState({typedValue: "",valueToType: this.props.currentText.charAt(this.currentPosition)});
         }
         //Update progress
         const tmpDone = this.props.currentText.substr(0,this.currentPosition);
-        const tmpRemaining = this.props.currentText.substr(this.currentPosition,this.props.currentTextLength);
-        this.setState({ doneText: tmpDone, remainingText: tmpRemaining});
+        const tmpRemaining = this.props.currentText.substr(this.currentPosition+1,this.props.currentTextLength);
+        this.setState({ 
+          doneText: tmpDone, 
+          remainingText: tmpRemaining});
       }
-      else { //Incorrect, play feedback, remove last letter from input
+      else { //Incorrect
         this.errors++;
-        const tmpStr = this.state.typedValue;
-        const cutStr = tmpStr.substr(0, tmpStr.length-1);
-        this.setState({typedValue: cutStr});
+
+        //todo: Play error audio feedback
       }
     }
   }
@@ -46,23 +55,26 @@ class Task extends React.Component {
   render() {
     return (
       <Fragment>
-        <h1>Tangentbordsträning</h1>
-        <label htmlFor="TypeHereForm">
-          <span className="done-text">{this.state.doneText}</span>
-          <span>{this.state.remainingText}</span>
-        </label>
-        <input type="text" 
-              name="TypeHereForm"
-              id="typeHereId" 
-              value={this.state.typedValue} 
-              onChange={this.handleChange}
-              onKeyUp={this.handleKey}
-              autoFocus>
-        </input>
+        <h2>Testing role="application"</h2>
+        <div 
+          className="typing-text-input" 
+          role="application" 
+          ref={this.input} 
+          tabIndex="0" 
+          onKeyUp={this.handleKey}
+          aria-label={'Type the text ' + this.state.valueToType}>
+            <span className="typing-text-input__typed-value">
+                {this.state.typedValue}
+            </span>
+            <span 
+              className="typing-text-input__value-to-type">
+                {this.state.valueToType}
+            </span>
+            <span>{this.state.remainingText}</span>
+        </div>
       </Fragment>
     );
   }
-
 }
 
 export default Task;
