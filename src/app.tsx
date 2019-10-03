16ce3140962634a77c13684a5220174713498cb2
duff-a11y <joakim@axesslab.com>
2019-10-03 10:04:14 +0200

    Fixed remaining code review issue + BEM css fixes

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import initStore from './config/store';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Task from './components/task';
 import Summary from './components/summary';
 
 const App: React.FC = (): React.ReactElement => {
-
-  const store = initStore();
-
   return (
-    <Provider store={store}>
-      <Router>
-        <Route exact path='/' render={props => <Task {...props} />} />
-        <Route exact path='/summary' render={props => <Summary {...props} />} />
-      </Router>
-    </Provider>
+    <Router>
+      <Route exact path='/' render={props => <Task {...props} />} />
+      <Route exact path='/summary' render={props => <Summary {...props} />} />
+    </Router>
   );
 }
 
--- src/components/summary/summary.tsx
+++ src/components/summary/summary.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import { IRootState } from '../../shared/reducers';
@@ -17,31 +17,30 @@ const Summmary = (props: React.PropsWithChildren<ISummmaryProps>) => {
       playAudio,
     } = props;
 
-    const feedback = (): string => {
+    const [feedbackText, setFeedbackText] = useState('');
+
+    useEffect(() => {
       if (taskErrors > 0) {
-        const wellDone = "Resultat.%20Bra%20jobbat!%20Du%20hade%20bara%20"+taskErrors+"%20fel!";
-        playAudio(  ['http://webbkonversation.se:59125/process?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT='+wellDone+'%0A&OUTPUT_TEXT=&VOICE_SELECTIONS=stts_sv_nst-hsmm%20sv%20male%20hmm&AUDIO_OUT=WAVE_FILE&LOCALE=sv&VOICE=stts_sv_nst-hsmm&AUDIO=WAVE_FILE',
+        const wellDone = "Resultat.%20Bra%20jobbat!%20Du%20hade%20bara%20" + taskErrors + "%20fel!";
+        playAudio(  ['http://webbkonversation.se:59125/process?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT=' + wellDone + 
+        '%0A&OUTPUT_TEXT=&VOICE_SELECTIONS=stts_sv_nst-hsmm%20sv%20male%20hmm&AUDIO_OUT=WAVE_FILE&LOCALE=sv&VOICE=stts_sv_nst-hsmm&AUDIO=WAVE_FILE',
         '/assets/done.mp3']);
-        return "Bra Jobbat! Du hade bara "+taskErrors+" fel!";
+        setFeedbackText("Bra Jobbat! Du hade bara " + taskErrors + " fel!");
       }
       const perfect = "Resultat.%20J%E4ttebra%20jobbat!%20Felfri!";
-      playAudio(  ['http://webbkonversation.se:59125/process?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT='+perfect+'%0A&OUTPUT_TEXT=&VOICE_SELECTIONS=stts_sv_nst-hsmm%20sv%20male%20hmm&AUDIO_OUT=WAVE_FILE&LOCALE=sv&VOICE=stts_sv_nst-hsmm&AUDIO=WAVE_FILE',
-          '/assets/done.mp3']);
-      return "Jättebra jobbat! Felfri!";
-    };
-
-    let feedbackText: string = feedback();
-
-    useEffect(()  => {
-      feedback();
-    }, []);
+      playAudio(  [
+        'http://webbkonversation.se:59125/process?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT=' + perfect + 
+        '%0A&OUTPUT_TEXT=&VOICE_SELECTIONS=stts_sv_nst-hsmm%20sv%20male%20hmm&AUDIO_OUT=WAVE_FILE&LOCALE=sv&VOICE=stts_sv_nst-hsmm&AUDIO=WAVE_FILE',
+        '/assets/done.mp3']);
+      setFeedbackText("Jättebra jobbat! Felfri!");
+    }, [feedbackText, playAudio, taskErrors]);
 
     return (
     <>
-      <div className="row flex-center pad-top-60-m pad-top-30">
-        <div className="status-text-divs">
+      <div className="flex-m flex-center pad-top-60-m pad-top-30">
+        <div className="summary__status">
           <h2>Resultat</h2>
-            {feedbackText}
+            <p>{feedbackText}</p>
             <AudioManager />
         </div>
       </div>
