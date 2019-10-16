ef7f6ad70e0cc61a1340d3aaeba198be51eff488
Emilia Michanek <emilia@axesslab.com>
2019-10-16 15:48:02 +0200

    Refactor code

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Task from './task';
+
 const Routes = ({ match }) => (
-  <>
-    <Switch>
-      <Route path={`${match.url}`} component={Task} />
-    </Switch>
-  </>
+  <Switch>
+    <Route path={`${match.url}`} component={Task} />
+  </Switch>
 );
 
-export default Routes; 
+export default Routes;
