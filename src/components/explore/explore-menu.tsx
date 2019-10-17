ef710a1f3b1b3f18e999d67d3b52e7134061a01b
duff-a11y <joakim@axesslab.com>
2019-10-17 14:29:22 +0200

    Removed unused import

--- src/components/explore/explore-menu.tsx
+++ src/components/explore/explore-menu.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { speak, ITTS, TTS_PLATTFORM } from '../tts/tts';
 import { playAudio } from '../audio/audio';
