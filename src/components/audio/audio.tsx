3002f6f8a6f1b1f29e0c308d4f2b339dff4be4dc
Erik Zetterström <erik@ictenablers.com>
2019-11-06 20:27:22 +0100

    Audio tweaks

--- src/components/audio/audio.tsx
+++ src/components/audio/audio.tsx
@@ -3,14 +3,16 @@ export const playAudio = async (audio, url: string, rate = 1): Promise<string> =
     return Promise.reject(`playAudio URL: ${url} audio: ${audio}`);
   } else {
     return new Promise((resolve, reject) => {
-      audio.current.playbackRate = rate;
-      audio.current.src = url;
       audio.current.pause();
+      audio.current.setAttribute('src','');
+      
+      audio.current.setAttribute('playbackRate', rate);
+      audio.current.setAttribute('src', url)  ;
       audio.current.load();
       audio.current.addEventListener('ended', resolve);
       audio.current.addEventListener('error', reject);
 
-      const promise = audio.current.play();
+      const promise = audio.current.play().catch(error => console.error('playAudio error '+error));
       if (promise !== undefined) {
         promise.catch(error => reject(error + ''));
       }
